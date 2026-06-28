e61165cee6e2d6f414422fa3fe743af68a33bb8a
Akash Gupta <zackgupta2@gmail.com>
2026-06-28 07:07:25 +0530

    Add files via upload

---
+++ student result system.py
@@ -0,0 +1,14 @@
+per = (input("enter marks numbar sub 1"))
+
+print("per =", per)
+
+if per >= '75':
+    print("Distinction")
+elif per >= '60':
+    print("first class")
+elif per >= '45':
+    print("second class")
+elif per >= '35' :
+    print("pass")
+else :
+    print("fail")
